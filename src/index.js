1d599d23f938845ce4b990d7b9d0553a91049262
jianxue <xjjingshi@163.com>
2024-04-21 21:05:05 +0800

    update

--- src/index.js
+++ src/index.js
@@ -12,13 +12,12 @@ const App = () => {
 
   useEffect(()=> {
     const unsubscribe = store.subscribe(()=> {
-
       const {count} = store.getState().addSubReducer
       console.log(count,'count')
       setMyCount(count)
     })
     return ()=> {
-      
+
       unsubscribe()
     }
   },[])
