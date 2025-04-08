8be81eaaceb003cf7076669bbb0e187e6bf87241
xjjingshi <xjjingshi@163.com>
2025-04-08 15:34:27 +0800

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,13 @@
 import { legacy_createStore as createStore } from 'redux'
 
 import combineReducers from './reducers'
+
 const store = createStore(combineReducers)
 
 store.subscribe(()=> {
 
     console.log(store.getState())
-    
+
 })
 
 
